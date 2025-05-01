a58d72dc419e91576ace40ed73801eaf1b1236a7
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-01 21:37:56 +0100

    Add: booking admin, code institute-CSRF

--- ember_ash/settings.py
+++ ember_ash/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
     dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net", "https://.herokuapp.com"
+]
 
 # TEMPORARILY COMMENTING OUT TO DISABLE
 # DATABASES = {
