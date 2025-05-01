6806887f7f6ad1fdb77a7eda0dc8a925f391733f
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-01 21:32:14 +0100

    Add: booking models

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Booking(models.Model):
+    booking_number = models.IntegerField(unique=True)
+    # user_id = models.IntegerField()
+    booking_date = models.DateField()
+    booking_time = models.TimeField()
+    guests_qty = models.IntegerField(default=1)
