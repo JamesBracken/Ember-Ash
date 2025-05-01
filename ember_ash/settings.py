3e28acc1aeadebd16c193a034a9069b9378fbb99
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-01 20:19:06 +0100

    Add: secret_key and connection

--- ember_ash/settings.py
+++ ember_ash/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ix$6@=qn#x(jpc&z6-%0^rynx3pm45y(jt$$zu0kci+*l)i!v&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
