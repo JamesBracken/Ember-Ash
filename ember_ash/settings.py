09530e7a8ea3c2ff943b671a9485373b22504896
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-01 18:56:00 +0100

    Add: booking app

--- ember_ash/settings.py
+++ ember_ash/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ix$6@=qn#x(jpc&z6-%0^rynx3pm45y(jt$$zu0kci+*l)i!v&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booking',
 ]
 
 MIDDLEWARE = [
