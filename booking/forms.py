53815bedabaa56c995db94ec71c9bd913c08de6d
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-05 20:45:25 +0100

    Fix: disable booking form previous dates

--- booking/forms.py
+++ booking/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import Booking
-
+import datetime
 class BookingForm(forms.ModelForm):
     """
     Creates a form for :model:`booking.Booking`
     """
-    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        today = datetime.date.today()
+        self.fields["booking_date"].widget.attrs["min"] = today
     class Meta:
         model = Booking
         fields = ("booking_date", "booking_time", "guests_qty", "comment")
+        widgets = {
+            "booking_date": forms.DateInput(attrs={"type": "date"}),
+        }
