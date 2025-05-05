8b1319978ba718bc433a665c6daabe3f3c9e791a
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-05 21:22:40 +0100

    Add: guest qty min max

--- booking/forms.py
+++ booking/forms.py
@@ -9,6 +9,10 @@ class BookingForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         today = datetime.date.today()
         self.fields["booking_date"].widget.attrs["min"] = today
+
+        # guest_qty widgets
+        self.fields["guests_qty"].widget.attrs["max"] = 100
+        self.fields["guests_qty"].widget.attrs["min"] = 1
     class Meta:
         # Taken from https://stackoverflow.com/questions/51164326/how-can-i-add-choices-to-a-timefield-in-a-django-form
         HOUR_CHOICES = [(datetime.time(hour=x), '{:02d}:00'.format(x)) for x in range(9, 23)]
@@ -16,5 +20,5 @@ class BookingForm(forms.ModelForm):
         fields = ("booking_date", "booking_time", "guests_qty", "comment")
         widgets = {
             "booking_date": forms.DateInput(attrs={"type": "date"}),
-            "booking_time": forms.Select(attrs={"type": "time"}, choices=HOUR_CHOICES)
+            "booking_time": forms.Select(attrs={"type": "time"}, choices=HOUR_CHOICES),
         }
