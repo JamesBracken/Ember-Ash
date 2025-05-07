f562376c314305490139c6ff48bd9a2018ede4d1
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-07 18:32:10 +0100

    Add: booking views & url

--- booking/views.py
+++ booking/views.py
@@ -7,8 +7,20 @@ from .forms import BookingForm
 
 # Creating forms.py then editing this content
 
-def Booking(request):
+def booking(request):
+    # latest_object = Status.objects.latest('date_added')
+
     # booking = Booking.objects.all()
+
+    # Defensive design incase object does not exist
+    # booking = get_object_or_404(queryset, slug=slug)
+
+
+    if request.method == "POST":
+        booking_form = BookingForm(data=request.POST)
+        if booking_form.is_valid():
+            booking_form.save(commit=False)
+            # Add messages
 
     booking_form = BookingForm()
     return render(
@@ -17,3 +29,7 @@ def Booking(request):
         {"booking_form": booking_form,
          }
     )
+
+
+        
+
