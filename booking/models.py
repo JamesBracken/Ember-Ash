a93e88023005b5591a1b33301d731b4e1d532959
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-07 18:31:07 +0100

    Add: booking models

--- booking/models.py
+++ booking/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 # from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Booking (models.Model):
-    booking_number = models.IntegerField(unique=True)
-    # user_id = models.IntegerField()
+    # Learned AutoField at https://www.geeksforgeeks.org/how-to-add-an-auto-increment-integer-field-in-django/
+    booking_number = models.AutoField(primary_key=True, unique=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
     booking_date = models.DateField()
     booking_time = models.TimeField()
     guests_qty = models.IntegerField(default=1)
