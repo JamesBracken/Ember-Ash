a97dc7658f2b9a989f8b1a5756125fc98bd40ccc
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-10 21:33:44 +0100

    Add: booking js global vars and structure

---
+++ static/js/booking.js
@@ -0,0 +1,21 @@
+// Global variables
+
+// Constants
+
+const editButtons = document.getElementsByClassName("edit-btn")
+const bookingForm = document.getElementById("bookingForm")
+const updateBookingBtn = document.getElementById("updateBookingBtn")
+// Inputs injected into my_profile template from booking forms.py
+const bookingDateInput = document.getElementById("id_booking_date")
+const bookingTimeInput = document.getElementById("id_booking_time")
+const bookingGuestsInput = document.getElementById("id_guests_qty")
+const bookingCommentInput = document.getElementById("id_comment")
+
+// Let and var variables
+
+// Event listeners
+
+// Functions
+
+// Naked code which doesn't fit into other categories
+
