52b77be04b28273f5e24717eb77ea5138aae95e8
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-10 15:34:28 +0100

    Fix: remove booking id from booking edit url path
    This reverts commit c49ac922e31ec518f679b868d3490f7f9b6bb4b1.

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('my_profile/', views.customer_profile, name='user_urls'),
-    # path('<slug:slug>/edit_booking/<int:booking_id>', views.booking_edit, name='booking_edit'),
+    # path('edit_booking/<slug:slug>/', views.booking_edit, name='booking_edit'),
     # path('my_profile/', views.customer_profile, name='customer_profile')
 ]
