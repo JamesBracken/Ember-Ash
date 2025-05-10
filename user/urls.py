b19a3ea6882ca42635e9584ab5dc18f417d982bb
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-10 16:04:40 +0100

    Fix: place new my profile url

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('my_profile/', views.customer_profile, name='user_urls'),
-    # path('edit_booking/<slug:slug>/', views.booking_edit, name='booking_edit'),
+    path('my_profile/', views.customer_profile, name='my_profile'),
+    path('edit_booking/<slug:slug>/', views.booking_edit, name='booking_edit'),
     # path('my_profile/', views.customer_profile, name='customer_profile')
 ]
