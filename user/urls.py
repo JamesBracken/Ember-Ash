c49ac922e31ec518f679b868d3490f7f9b6bb4b1
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-10 15:06:35 +0100

    Add: customer booking edit url

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('my_profile/', views.customer_profile, name='user_urls')
+    path('my_profile/', views.customer_profile, name='user_urls'),
+    # path('<slug:slug>/edit_booking/<int:booking_id>', views.booking_edit, name='booking_edit'),
     # path('my_profile/', views.customer_profile, name='customer_profile')
 ]
