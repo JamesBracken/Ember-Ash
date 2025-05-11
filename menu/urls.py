56e86a92e43a6d8b49631c046a264604b8340a9a
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-11 23:25:08 +0100

    Add: menu form

--- menu/urls.py
+++ menu/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('menu/breakfast_menu/', views.breakfast_menu, name='breakfast_menu'),
     path('menu/dinner_menu/', views.dinner_menu, name='dinner_menu'),
+    path('menu/menu_form/', views.add_menu_item, name='add_menu_item'),
 ]
