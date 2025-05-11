02cb94a833c74d2cb852965de8f77139e3684a99
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-11 17:41:36 +0100

    Add: pagination for my profile

--- user/views.py
+++ user/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from booking.models import Booking
 from booking.forms import BookingForm
 from django.http import HttpResponseRedirect
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -14,11 +14,16 @@ def customer_profile(request):
     # template_name = "user/my_profile.html"
     # Consider using Paginate
     booking_form = BookingForm(data=request.POST)
+    paginator = Paginator(user_bookings, 5)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+
     return render(
         request,
         "my_profile.html",
         {
-            "bookings": user_bookings,
+            # "bookings": user_bookings,
+            "page_object": page_object,
             "booking_form": booking_form,
             "user": request.user,
         }
