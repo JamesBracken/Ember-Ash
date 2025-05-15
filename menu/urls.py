ec9ef76eb267c62857312ee3df275f08446b1d61
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-15 16:12:33 +0100

    Fix: menu urls hierarchy and organization

--- menu/urls.py
+++ menu/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('menu/lunch_menu/', views.lunch_menu, name='lunch_menu'),
+    path('menu/lunch_menu/delete_booking/<int:id>', views.delete_menu_item, name='menu_delete'),
     # path('menu/lunch_menu/edit_menu_item/<slug:slug>/', views.edit_menu_item, name='lunch_menu_edit'),
     path('menu/dinner_menu/', views.dinner_menu, name='dinner_menu'),
+    path('menu/dinner_menu/delete_booking/<int:id>', views.delete_menu_item, name='menu_delete'),
     path('menu/menu_form/', views.add_menu_item, name='add_menu_item'),
     path('menu/menu_form/<slug:slug>', views.edit_menu_item, name='menu_edit'),
-    path('menu/dinner_menu/delete_booking/<int:id>', views.delete_menu_item, name='menu_delete'),
 ]
-# delete_menu_item
