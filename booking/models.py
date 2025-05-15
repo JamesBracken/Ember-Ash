a29337b8bfdc955c1618fea453e752b033a7693d
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-15 21:53:09 +0100

    Fix: code hierarchy, comments DEL: commented out code Add: view decorators

--- booking/models.py
+++ booking/models.py
@@ -8,11 +8,7 @@ class Booking (models.Model):
     """
     Stores a booking entry to :model:`booking.Booking`.
     """
-    # Learned AutoField at https://www.geeksforgeeks.org/how-to-add-an-auto-increment-integer-field-in-django/
-    # booking_number = models.AutoField(primary_key=True, unique=True)
-    # TEMPORARILY COMMENTING OUT
     slug = models.SlugField(max_length=200, unique=True)
-    # slug = models.SlugField(max_length=200, null=True, unique=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
     booking_date = models.DateField()
     booking_time = models.TimeField()
@@ -26,8 +22,6 @@ class Booking (models.Model):
             self.slug = slugify(str(self.id))
             return super().save(*args, **kwargs)
         super().save(*args, **kwargs)
-
-
 
     # def __str__(self):
         # return f"Booking for {self.user_id.first_name} on {self.booking_date}"
--- booking/views.py
+++ booking/views.py
@@ -3,11 +3,10 @@ from .models import Booking
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .forms import BookingForm
+from django.contrib.auth import login_required
 
 # Create your views here.
-
-# Creating forms.py then editing this content
-
+@login_required
 def booking(request):
     """
     Displays a form to create an instance of :model:`booking.Booking`
@@ -17,42 +16,36 @@ def booking(request):
     ``booking_form``
         An instance of :forms:`booking.BookingForm`
     """
-    # latest_object = Status.objects.latest('date_added')
-
-    # booking = Booking.objects.all()
-
-    # Defensive design incase object does not exist
-    # booking = get_object_or_404(queryset, slug=slug)
-
     if request.method == "POST":
         booking_form = BookingForm(data=request.POST)
         if booking_form.is_valid():
             booking = booking_form.save(commit=False)
             booking.user = request.user
             booking.save()
-            # Add messages
+            # Adds a success message
             messages.add_message(
-                request, messages.SUCCESS,
-                "Booking successfully created, you may view this in your My Profile area"
+                request,
+                messages.SUCCESS,
+                "Booking successfully created, you may view this in your My Profile area",
             )
             return redirect("home_urls")
     else:
         booking_form = BookingForm()
-        
     return render(
         request,
         "booking/booking_form.html",
-        {"booking_form": booking_form,
-        }
+        {
+            "booking_form": booking_form,
+        },
     )
 
-
+@login_required
 def booking_edit(request, slug):
     """
     Displays an individual booking for edit.
 
     **Context**
-    
+
     ``booking_form``
         An instance of :forms:`booking.BookingForm`
     ``booking``
@@ -72,7 +65,7 @@ def booking_edit(request, slug):
             messages.add_messages(request, messages.ERROR, "Error updating booking")
         return HttpResponseRedirect(reverse("my_profile"))
 
-
+@login_required
 def booking_delete(request, slug):
     """
     Deletes an individual selected instance of :model:`booking.Booking`
@@ -89,14 +82,18 @@ def booking_delete(request, slug):
         booking.delete()
         messages.add_message(request, messages.SUCCESS, "Booking was deleted!")
     else:
-        messages.add_message(request, messages.ERROR, "You can only delete your own bookings!")
+        messages.add_message(
+            request, messages.ERROR, "You can only delete your own bookings!"
+        )
 
     return HttpResponseRedirect(reverse("my_profile"))
+
 
 def trigger_login_message(request):
     """
     Displays a message if a user is not authenticated and attempts to make a booking
     """
-    # messages.ERROR(request, "You must login first before making a booking")
-    messages.add_message(request, messages.SUCCESS, "You must login first before making a booking" )
-    return redirect('home_urls')
+    messages.add_message(
+        request, messages.SUCCESS, "You must login first before making a booking"
+    )
+    return redirect("home_urls")
