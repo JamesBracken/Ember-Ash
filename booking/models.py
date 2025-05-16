9b63ce8024df105e374bd52d328b89085c37c709
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-16 21:58:49 +0100

    DEL: booking slug

--- booking/models.py
+++ booking/models.py
@@ -8,20 +8,11 @@ class Booking (models.Model):
     """
     Stores a booking entry to :model:`booking.Booking`.
     """
-    slug = models.SlugField(max_length=200, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
     booking_date = models.DateField()
     booking_time = models.TimeField()
     guests_qty = models.IntegerField(default=1)
     comment = models.CharField(max_length=200, blank=True)
-
-# Set slug to booking id (unique)
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            super().save(*args, **kwargs)
-            self.slug = slugify(str(self.id))
-            return super().save(*args, **kwargs)
-        super().save(*args, **kwargs)
 
     # def __str__(self):
         # return f"Booking for {self.user_id.first_name} on {self.booking_date}"
