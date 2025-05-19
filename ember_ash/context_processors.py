048ef985ac5537e20ea9bb5e7ac626a9cfd9dc0b
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-19 18:54:30 +0100

    Fix: Login modal form error handling

--- ember_ash/context_processors.py
+++ ember_ash/context_processors.py
@@ -1,9 +1,9 @@
 from allauth.account.forms import LoginForm
 from allauth.account.forms import SignupForm
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def login_form(request):
-    return {'login_form': LoginForm()}
+    return {'login_form': AuthenticationForm()}
 
 def signup_form(request):
-    return {'signup_form': SignupForm()}
+    return {'signup_form': UserCreationForm()}
--- static/js/login.js
+++ static/js/login.js
@@ -15,7 +15,11 @@ function handleLogin(e) {
     // Create new instance of form data on each submit click
     // to pass to validation
     const formData = new FormData(loginForm)
+    // console.log("FormData:" + [...formData.entries()])
 
+    for(const[key, value] of formData.entries()) {
+        console.log(`${key}: ${value}`)
+    }
     fetch(loginForm.action, {
         method: "POST",
         headers: {
@@ -23,26 +27,27 @@ function handleLogin(e) {
         },
         body: formData
     })
-    .then(response => response.json())
-    .then(data => {
-        if(data.success) {
-            window.location.href = data.home_url
-        } else {
-            const loginErrorsDisplay = document.getElementsByClassName("login-username-errors")
-            console.log(loginErrorsDisplay)
-            console.log("Errors: " + data.errors)
-            const errors = JSON.parse(data.errors);
-            for (let field in errors) {
-                errors[field].forEach(error => {
-                    console.log("Errors within for loop: " + error)
-                    const p = document.createElement("p");
-                    p.textContent = error.message;
-                    loginErrorsDisplay.appendChild(p)
-                })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                window.location.href = data.home_url
+            } else {
+                const loginErrorsDisplay = document.getElementsByClassName("login-errors")[0]
+                console.log(loginErrorsDisplay)
+                console.log("Errors: " + data.errors)
+                loginErrorsDisplay.innerHTML = ''
+                const errors = JSON.parse(data.errors);
+                for (let field in errors) {
+                    errors[field].forEach(error => {
+                        console.log("Errors within for loop: " + error)
+                        const p = document.createElement("p");
+                        p.textContent = error.message;
+                        loginErrorsDisplay.appendChild(p)
+                    })
+                }
             }
-        }
-    })
-    .catch(error => console.error("Error:", error))
+        })
+        .catch(error => console.error("Error:", error))
 }
 // NAKED CODE which doesn't fit into other categories
 
