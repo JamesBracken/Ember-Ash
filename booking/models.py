c4f3504cf14f6b9d2beb02e4136d8260bc19e5d4
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-20 19:31:10 +0100

    Fix: menu docstrings, removed comments

--- booking/models.py
+++ booking/models.py
@@ -1,17 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Booking (models.Model):
     """
-    Stores a booking entry to :model:`booking.Booking`.
+    Stores a booking entry related to :model:`auth.user`.
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
     booking_date = models.DateField()
     booking_time = models.TimeField()
     guests_qty = models.IntegerField(default=1)
     comment = models.CharField(max_length=200, blank=True)
-
-    # def __str__(self):
-        # return f"Booking for {self.user_id.first_name} on {self.booking_date}"
