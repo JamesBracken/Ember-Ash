537b5380c9e60120ea1b58238b762653173b6596
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-21 15:27:39 +0100

    Add: allauth signup page

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Booking
 from django.contrib import messages
 from .forms import BookingForm
