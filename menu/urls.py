ddf9963cb43f22182112bf502c03d1916b7a44f3
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-05-25 13:52:38 +0100

    Fix: Add active class on lunch and dinner page

--- menu/urls.py
+++ menu/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     ),
     path("menu/dinner_menu/", views.dinner_menu, name="dinner_menu"),
     path("menu/menu_form/", views.add_menu_item, name="add_menu_item"),
-    path("menu/menu_form/<slug:slug>", views.edit_menu_item, name="menu_edit"),
+    path("menu/menu_form/<slug:slug>", views.edit_menu_item, name="menu_edit")
 ]
