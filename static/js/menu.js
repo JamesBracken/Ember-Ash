964afcf2c6843fecd3a2742236192e9b606c5cdb
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-14 15:44:33 +0100

    Fix: correct js Doctstring

--- static/js/menu.js
+++ static/js/menu.js
@@ -9,11 +9,9 @@ for (let button of deleteButtons) {
 }
 
 // FUNCTIONS
-/* This function deletes the selected menu item, using the event handler, it takes the
-id of the selected booking and deletes the data connected with this id. 
-Before deletion a confirmation modal
-is displayed for the user to confirm menu item deletion and warns the user this 
-cannot be undone
+/* This function opens the delete confirmation modal, it takes the
+id of the selected menu item and it updates the href for the delete_menu_item
+ view to delete the correct data. 
 
 *@param {Click} e - This is information of the event that triggers the
  function, using this we get the data-booking_id attribute to delete the 
